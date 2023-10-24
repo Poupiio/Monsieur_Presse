28b59a8513635b6c8482ac16479a125c577b840c
Poupiio <elise.maaiza33@gmail.com>
2023-10-24 14:09:36 +0200

    Ajout titre

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const boxes = document.querySelectorAll('.box');
 const eye = document.querySelector('.eye');
+const title = document.querySelector('.title');
+
+let purpleCount = 0;
+let yellowCount = 0;
+const totalBoxes = 21;
+const hoveredBoxes = new Set(); // Ensemble pour stocker les cases survolées
 
 boxes.forEach((box) => {
     box.addEventListener('mouseover', () => {
-        if (box.id == "yellow") {
-            box.style.cursor = "pointer";
-            box.style.backgroundColor = "yellow";
-        } else if (box.classList.contains("purple") || box.classList.contains("eye")) {
-            box.style.cursor = "pointer";
-            box.style.backgroundColor = "purple";
+        if (!hoveredBoxes.has(box)) {
+            // Si la case n'a pas été survolée auparavant
+            hoveredBoxes.add(box); // Ajoute la case à l'ensemble
+
+            if (box.id == "yellow") {
+                box.style.cursor = "pointer";
+                box.style.backgroundColor = "yellow";
+                yellowCount++;
+            } else if (box.classList.contains("purple") || box.classList.contains("eye")) {
+                box.style.cursor = "pointer";
+                box.style.backgroundColor = "purple";
+                purpleCount++;
+            }
+
+            if (purpleCount + yellowCount == totalBoxes) {
+                title.style.opacity = "1";
+            }
         }
     })
 
@@ -17,4 +34,4 @@ boxes.forEach((box) => {
             eye.style.visibility = "visible";
         });
     }
-})
+});
